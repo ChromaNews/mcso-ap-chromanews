e9c855f20e945770224cc80a5efcab2e7495a419
Thiyagarajan Angappan <thiyagarajan.angappan@gmail.com>
2022-11-17 23:27:05 -0600

    add testing code to calc color

--- app/src/main/java/com/mcso/ap/chromanews/MainActivity.kt
+++ app/src/main/java/com/mcso/ap/chromanews/MainActivity.kt
@@ -48,12 +48,11 @@ class MainActivity : AppCompatActivity() {
         AuthInit(viewModel,signInLauncher)
 
         // test firestore
-        /*viewModel.updateUserSentiment()
         viewModel.calculateRating()
 
         viewModel.observeRatingByDate().observe(this){
             viewModel.calculateSentimentColorCode(it)
-        }*/
+        }
 
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.recyclerView)
