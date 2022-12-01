d46e7b95de895cc50bcedb520b8db835eb917404
Thiyagarajan Angappan <thiyagarajan.angappan@gmail.com>
2022-12-01 12:33:27 -0600

    handle exception for network calls

--- app/src/main/java/com/mcso/ap/chromanews/api/ConflictsRepo.kt
+++ app/src/main/java/com/mcso/ap/chromanews/api/ConflictsRepo.kt
@@ -1,9 +1,19 @@
 package com.mcso.ap.chromanews.api
 
+import android.util.Log
+import com.mcso.ap.chromanews.model.conflict.Conflicts
 import com.mcso.ap.chromanews.model.conflict.ConflictsResponse
 
 class ConflictRepo(private val api: ConflictsApi) {
     suspend fun getConflictData(country: String): ConflictsResponse {
-        return api.getConflictData(country)
+
+        var response = ConflictsResponse(0, 0, mutableListOf<Conflicts>())
+
+        try {
+            response = api.getConflictData(country)
+        } catch (e: Exception){
+            Log.e(javaClass.simpleName, "Exception while fetching conflict data: ${e.message}")
+        }
+        return response
     }
 }
