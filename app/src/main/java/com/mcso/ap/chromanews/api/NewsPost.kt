0708003f0f1455f11f660e18522b799ac51ecdc3
Anbarasan <anbarasanm@utexas.edu>
2022-12-02 10:32:53 -0700

    Removed unused spans code

--- app/src/main/java/com/mcso/ap/chromanews/api/NewsPost.kt
+++ app/src/main/java/com/mcso/ap/chromanews/api/NewsPost.kt
@@ -1,9 +1,5 @@
 package com.mcso.ap.chromanews.api
 
-import android.graphics.Color
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
 import com.google.gson.annotations.SerializedName
 
 data class NewsPost (
@@ -22,32 +18,6 @@ data class NewsPost (
     @SerializedName("urlToImage")
     val imageURL : String?,
 ) {
-    companion object {
-        // NB: This only highlights the first match in a string
-        private fun findAndSetSpan(fulltext: SpannableString, subtext: String): Boolean {
-            if (subtext.isEmpty()) return true
-            val i = fulltext.indexOf(subtext, ignoreCase = true)
-            if (i == -1) return false
-            fulltext.setSpan(
-                ForegroundColorSpan(Color.CYAN), i, i + subtext.length,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-            return true
-        }
-
-        fun spannableStringsEqual(a: SpannableString?, b: SpannableString?): Boolean {
-            if(a == null && b == null) return true
-            if(a == null && b != null) return false
-            if(a != null && b == null) return false
-            val spA = a!!.getSpans(0, a.length, Any::class.java)
-            val spB = b!!.getSpans(0, b.length, Any::class.java)
-            return a.toString() == b.toString()
-                    &&
-                    spA.size == spB.size && spA.equals(spB)
-
-        }
-    }
-
     override fun equals(other: Any?) : Boolean =
         if (other is NewsPost) {
             title == other.title
