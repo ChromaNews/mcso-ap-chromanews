3c12acdf3bd8904997652ee119232b7d87f9a924
Thiyagarajan Angappan <thiyagarajan.angappan@gmail.com>
2022-12-02 13:37:06 -0600

    remove unnecessary logs

--- app/src/main/java/com/mcso/ap/chromanews/ui/newsfeed/ScienceFragment.kt
+++ app/src/main/java/com/mcso/ap/chromanews/ui/newsfeed/ScienceFragment.kt
@@ -21,14 +21,12 @@ class ScienceFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        Log.d(javaClass.simpleName, "ScienceFragment onCreateView")
         _binding = FragmentRvBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(javaClass.simpleName, "ScienceFragment onViewCreated")
 
         binding.recyclerRVView.layoutManager = LinearLayoutManager(binding.recyclerRVView.context)
         val adapter = NewsFeedAdapter(viewModel)
--- app/src/main/java/com/mcso/ap/chromanews/ui/newsfeed/SportsFragment.kt
+++ app/src/main/java/com/mcso/ap/chromanews/ui/newsfeed/SportsFragment.kt
@@ -21,14 +21,12 @@ class SportsFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        Log.d(javaClass.simpleName, "SportsFragment onCreateView")
         _binding = FragmentRvBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(javaClass.simpleName, "SportsFragment onViewCreated")
 
         binding.recyclerRVView.layoutManager = LinearLayoutManager(binding.recyclerRVView.context)
         val adapter = NewsFeedAdapter(viewModel)
